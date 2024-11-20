c322aef04c23d2766373344b93b5f04a1dd80c52
Rodrigo <rodrigo.montoza@gmail.com>
2024-11-20 18:59:55 -0300

    inclusão do migration no start

--- src/adapter/driven/infra/interfaces/IDatabase.ts
+++ src/adapter/driven/infra/interfaces/IDatabase.ts
@@ -1,5 +1,6 @@
 interface IDatabase {
-    connect(): Promise<void>;
-  }
-  
-  export { IDatabase };
+  connect(): Promise<void>;
+  query(sql: string, params?: any[]): Promise<void>; // Parâmetro `params` opcional
+}
+
+export { IDatabase };
